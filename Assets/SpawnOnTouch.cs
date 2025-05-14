4eb61933607da1d18e21165fbf46dba62af75552
Sebastian <sebander01@gmail.com>
2025-05-14 00:56:09 -0400

    AddedASpawningZone

--- Assets/SpawnOnTouch.cs
+++ Assets/SpawnOnTouch.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.iOS;
 using UnityEngine.Rendering.Universal;
 
 public class SpawnOnTouch : MonoBehaviour
@@ -63,16 +64,52 @@ public class SpawnOnTouch : MonoBehaviour
         singleClick.Disable();
     }
 
+    /// <summary>
+    /// Spawns our player object balls
+    /// </summary>
     private void SpawnObject()
     {
+            //If we are allowed to spawn and we have and inventory of balls left
             if (CanSpawn && ballCount > 0)
             {
+                //We can't spawn
                 CanSpawn = false;
+                //Collect the position of the touch
                 Vector2 touchPosition = singleTouch.ReadValue<Vector2>();
+                //Translate to screen space to world space
                 touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+                
+                
+                //Transform the object variable position based on the translated screen space
                 shapeToSpawn.transform.position = new Vector2(touchPosition.x, touchPosition.y);
-                Instantiate(shapeToSpawn);
-                ballCount--;
+                //Validate the screen space
+                if (ValidatePlayArea(shapeToSpawn.transform))
+                {
+                    //Spawn the object
+                    Instantiate(shapeToSpawn);
+                    ballCount--;
+                }
+                else
+                {
+                    Debug.Log("Not in play area");
+                }
             }
+    }
+
+    /// <summary>
+    /// Checks if an object is being spawned somewhere that is valid. The target is the game object that would be summoned if allowed
+    /// </summary>
+    private bool ValidatePlayArea(Transform target)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(target.position, this.transform.position);
+        if (hit.collider.tag == "PlayZone")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 }
